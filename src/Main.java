3f5450e772318df78229524d32a8ffd14e55566a
numme <nummer14@naver.com>
2025-06-13 10:34:55 +0900

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Person {
+    public String name;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.name = "h662";
+
+        System.out.println(person.name);
+    }
+}
